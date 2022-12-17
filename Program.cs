e1db31d8d1edf7b2a62ba1243983f1c9eaba635c
daliawahdan <120822037+daliawahdan@users.noreply.github.com>
2022-12-17 20:32:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("please enter the 1st no");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("please enter the 2nd no");
+int y = int.Parse(Console.ReadLine());
+
+for(int i = x ; i <= y ; i++)
+{
+    int sum = 0;
+    for (int j = 1; j < i; j++)
+    {
+        if (i % j == 0)
+            sum = sum + j;
+    }
+    if (sum == i)
+        Console.WriteLine("\t" + i);
+}    
+
